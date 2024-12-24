daf4f63465cba6c2c4c501d0d22c9ce5a920e040
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-12-24 15:46:52 +0100

    menu 2.0

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,6 @@ const toggle = document.querySelector(".header__menu-btn");
 const nav = document.querySelector(".header__menu");
 const page = document.body;
 
-document.querySelector(".header__menu-btn").addEventListener("click", () => {
-    console.log("Bouton cliqué !");
-});
-
-
 if (toggle && nav) {
     toggle.addEventListener("click", () => {
         const isOpen = toggle.ariaExpanded === "true";
