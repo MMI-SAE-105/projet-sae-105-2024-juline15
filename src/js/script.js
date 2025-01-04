5c1d6319579447cb448f10549267a938d9ac56c3
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-01-04 10:46:16 +0100

    carrousel

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,21 @@ if (toggle && nav) {
         page.classList.toggle("noscroll", isClosed);
     });
 }
+
+/* carrousel */
+const carousel = document.querySelector(".film-gallery__carousel");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const premierItem = document.querySelector(".film-gallery__card");
+
+if (carousel && prevButton && nextButton && premierItem) {
+    prevButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth + 20;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+
+    nextButton.addEventListener("click", () => {
+        const scrollAmount = premierItem.clientWidth + 20;
+        carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+}
