d50a756e8b786ad7b33b8f51e1f7b515d0200a6d
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-01-04 16:18:00 +0100

    lightbox sur le carrousel

--- src/js/script.js
+++ src/js/script.js
@@ -30,3 +30,27 @@ if (carousel && prevButton && nextButton && premierItem) {
         carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
     });
 }
+
+/* lightbox */
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox > img");
+
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches(".film-gallery__image")) {
+        lightboxImg.src = evt.target.src;
+        lightbox.classList.add("entrée");
+        lightbox.showModal();
+    }
+});
+
+lightbox.addEventListener("click", (evt) => {
+    if (evt.target === lightbox) {
+        lightbox.classList.add("sortie");
+        lightbox.addEventListener("animationend", () => {
+            lightbox.classList.remove("sortie", "entrée");
+            lightbox.close();
+        }, { once: true });
+    }
+});
+
+
